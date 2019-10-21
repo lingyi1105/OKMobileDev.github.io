7240dfda3f9b6c1b40c1ec0c4bb2535394e97d02
金小白 <xiaolei.jin@okcoin.net>
2019-10-21 15:55:28 +0800

    国际化调试通过

--- download/test/index.js
+++ download/test/index.js
@@ -1,27 +1,62 @@
+const messages = {
+    // 'lang': path.i18n,
+    zh: {
+        headerTitle: '全球领先的数字资产交易平台',
+        headerSubtitle: '安全 · 稳定 · 可信',
+        androidInstallTitle: '安装 Android 版',
+        betaTitle: '安装内测版',
+        title1: '更安全',
+        subtitle1:'老牌交易所',
+        title2: '更多糖果',
+        subtitle2:'多元化奖励机制',
+        title3: '更多样',
+        subtitle3:'支持合约等多种交易类型',
+    },
+    en: {
+        headerTitle: '111',
+        headerSubtitle: '222',
+        androidInstallTitle: '333',
+        betaTitle: '444',
+        title1: '555',
+        subtitle1:'666',
+        title2: '777',
+        subtitle2:'888',
+        title3: '999',
+        subtitle3:'000',
+    }
+}
+
+const i18n = new VueI18n({
+    locale: ((navigator.language)?navigator.language:navigator.userLanguage).substr(0, 2), // 设置语言
+    // locale: 'en',
+    fallbackLocale: 'en',// 获取不到语言的话默认的语言
+    messages // 语言包
+})
+
 new Vue({
     el: '#app',
     data: function () {
         return {
             headerName: 'OKEX',
-            headerTitle: '全球领先的数字资产交易平台',
-            headerSubtitle: '安全 · 稳定 · 可信',
-            androidInstallTitle: '安装 Android 版',
-            betaTitle: '安装内测版',
+            headerTitle: i18n.t('headerTitle'),
+            headerSubtitle: i18n.t('headerSubtitle'),
+            androidInstallTitle: i18n.t('androidInstallTitle'),
+            betaTitle: i18n.t('betaTitle'),
             storeTitle: 'App Store',
             logo: './images/logo.png',
             androidDownloadURL: 'https://ok-public-hz.oss-cn-hangzhou.aliyuncs.com/upgradeapp/OKEx-android.apk',
             tipsData: [{
                 icon: './icon/icon-security.png',
-                title: '更安全',
-                subtitle: '老牌交易所',
+                title: i18n.t('title1'),
+                subtitle: i18n.t('subtitle1'),
             }, {
                 icon: './icon/icon-candy.png',
-                title: '更多糖果',
-                subtitle: '多元化奖励机制',
+                title: i18n.t('title2'),
+                subtitle: i18n.t('subtitle2'),
             }, {
                 icon: './icon/icon-various.png',
-                title: '更多样',
-                subtitle: '支持合约等多种交易类型',
+                title: i18n.t('title3'),
+                subtitle: i18n.t('subtitle3'),
             }]
         }
     },
