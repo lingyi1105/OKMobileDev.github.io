4ddc96d2de2459c2e398ee4414104e229a4e99e6
金小白 <xiaolei.jin@okcoin.net>
2019-10-25 16:26:28 +0800

    CoinAll 适配海外用户，支持英文

--- download/coinall/index.js
+++ download/coinall/index.js
@@ -1,30 +1,62 @@
+const messages = {
+    zh: {
+        headerTitle: '币全 CoinAll 数字货币交易平台',
+        headerSubtitle: '安全便捷 · 优质币种',
+        androidInstallTitle: '立即安装 CoinAll',
+        betaTitle: '立即安装 CoinAll',
+        title1: '高效的撮合系统',
+        title2: '安全的钱包服务',
+        title3: '稳定的资金清算系统',
+        title4: '全球化客服服务',
+        title5: '共享深度和流动性',
+    },
+    en: {
+        headerTitle: 'Safe and Quality Ethereum\nexchange Bitcoin wallet site',
+        headerSubtitle: ' ',
+        androidInstallTitle: 'Download CoinAll',
+        betaTitle: 'Download CoinAll',
+        title1: 'Efficient Order Matching System',
+        title2: 'Banking-grade Wallet',
+        title3: 'Leading Risk Management System',
+        title4: 'Global customer support',
+        title5: 'Share Liquidity With OKEx',
+    }
+}
+
+const i18n = new VueI18n({
+    locale: ((navigator.language) ? navigator.language : navigator.userLanguage).substr(0, 2), // 设置语言
+    // locale: 'en',
+    fallbackLocale: 'en',// 获取不到语言的话默认的语言
+    messages // 语言包
+})
+
 new Vue({
     el: '#app',
     data: function () {
         return {
             headerName: ' ',
-            headerTitle: '币全 CoinAll 数字货币交易平台',
-            headerSubtitle: '安全便捷 · 优质币种',
-            androidInstallTitle: '立即安装 CoinAll',
-            betaTitle: '立即安装 CoinAll',
+            headerTitle: i18n.t('headerTitle'),
+            headerSubtitle: i18n.t('headerSubtitle'),
+            androidInstallTitle: i18n.t('androidInstallTitle'),
+            betaTitle: i18n.t('betaTitle'),
             storeTitle: false,
             logo: './images/header-logo.png',
             androidDownloadURL: 'http://upgradeapp.oss-cn-hangzhou.aliyuncs.com/upgradeapp/CoinAll-android.apk',
             tipsData: [{
                 icon: './icon/icon-1.png',
-                title: '高效的撮合系统',
+                title: i18n.t('title1'),
             }, {
                 icon: './icon/icon-2.png',
-                title: '安全的钱包服务',
+                title: i18n.t('title2'),
             }, {
                 icon: './icon/icon-3.png',
-                title: '稳定的资金清算系统',
+                title: i18n.t('title3'),
             }, {
                 icon: './icon/icon-4.png',
-                title: '全球化客服服务',
+                title: i18n.t('title4'),
             }, {
                 icon: './icon/icon-5.png',
-                title: '共享深度和流动性',
+                title: i18n.t('title5'),
             }]
         }
     },
