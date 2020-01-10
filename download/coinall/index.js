987c86360812d2af576249b5913646958670c954
金小白 <xiaolei.jin@okcoin.net>
2020-01-10 17:15:44 +0800

    修改 CoinAll iOS 页面

--- download/coinall/index.js
+++ download/coinall/index.js
@@ -9,6 +9,7 @@ const messages = {
         title3: '稳定的资金清算系统',
         title4: '全球化客服服务',
         title5: '共享深度和流动性',
+        testflightTips: '<div>为给您提供更加稳定的服务，iOS 版本已升级到 TestFlignt 测试版，使用稳定无掉签困扰。</div><div>在您点击安装应用后需按提示<b style=font-size: 13px;>先下载 TestFlignt</b>，这不会对您的设备安全产生任何影响，请安心下载。</div>',
     },
     en: {
         headerTitle: 'Safe and Quality Ethereum\nexchange Bitcoin wallet site',
@@ -20,6 +21,7 @@ const messages = {
         title3: 'Leading Risk Management System',
         title4: 'Global customer support',
         title5: 'Share Liquidity With OKEx',
+        testflightTips: '<div>In order to offer users more stable services, the CoinAll app (iOS version) has been upgraded to TestFlignt beta for a smoother operation. </div><div>After clicking install, <b style=font-size: 13px;>please follow the instructions to download TestFlignt</b>. Please be rest assured that this will not cause any security risks to your device.</div>',
     }
 }
 
@@ -42,6 +44,7 @@ new Vue({
             storeTitle: false,
             logo: './images/header-logo.png',
             androidDownloadURL: 'https://hkpublic.oss-cn-hongkong.aliyuncs.com/upgradeapp/CoinAll-android.apk',
+            testflightTips: i18n.t('testflightTips'),
             tipsData: [{
                 icon: './icon/icon-1.png',
                 title: i18n.t('title1'),
